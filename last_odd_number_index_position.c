0a3f41d2e49e3c90482df7c71aa9cb2249ad3621
Sivaram18162 <97424725+Sivaram18162@users.noreply.github.com>
2022-04-28 20:24:02 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==1)         {             c=1;             printf("%d",i);             break;         }     }     return 0; }

--- last_odd_number_index_position.c
+++ last_odd_number_index_position.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],c;
+    int n,i,arr[100],c=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=n-1;i>=0;i++)
+    for(i=n-1;i>=0;i--)
     {
         if(arr[i]%2==1)
         {
