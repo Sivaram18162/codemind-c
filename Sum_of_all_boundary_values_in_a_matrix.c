42dd11d51b70f8bd3b917f6c21ac44c92c5c31f0
Sivaram18162 <97424725+Sivaram18162@users.noreply.github.com>
2022-06-29 19:26:09 +0530

    #include<stdio.h> int main() {     int a,b,i,j,s=0,arr[100][100];     scanf("%d%d",&a,&b);     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(i=0;i<a;i++)     {         for(j=0;j<b;j++)         {             if(i==0 || j==0 || i==a-1 || j==b-1)             {                s=s+arr[i][j];             }         }     }     printf("%d",s); }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,s=0,arr[100][100];
+    scanf("%d%d",&a,&b);
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        for(j=0;j<b;j++)
+        {
+            if(i==0 || j==0 || i==a-1 || j==b-1)
+            {
+               s=s+arr[i][j];
+            }
+        }
+    }
+    printf("%d",s);
+}
